c4085445de8bdfaa99f9be14f8d722572f7df6e7
helenmiller16 <helen@ohana.org>
2019-05-13 17:47:06 -0700

    Changed data structure

--- helpers/d3heatmap.R
+++ helpers/d3heatmap.R
@@ -6,23 +6,15 @@ d3Heatmap <- function(data) {
                         "13", "14", "15-27", "28", "29-55", "56", ">56")
   assays <- unique(hmdata$assay)[order(unique(hmdata$assay), decreasing = TRUE)]
   
-  # Get colors
-  colors = c("#FFFFFF", RColorBrewer::brewer.pal(6, "Greens"))
-  hmdata$colorIndex <- .bincode(hmdata$count, breaks = c(0, 10, 50, 100, 500, 1000, Inf), include.lowest = TRUE) + 1
-  hmdata$colorIndex <- ifelse(hmdata$count == 0, 1, hmdata$colorIndex)
-  hmdata$color <- colors[hmdata$colorIndex]
   
   d <- hmdata
-  d3data <- 
-    vapply(timepoints_xaxis, function(tp) {
-      lapply(assays, function(as) {
-        list(participantCount = d[timepoint == tp & assay == as, count],
-             studies = unlist(d[timepoint == tp & assay == as, studyList]),
-             color = d[timepoint == tp & assay == as, color],
-             assay = as,
-             timepoint = tp)
-      })
-    }, FUN.VALUE = rep(list(list()), length(assays)))
+  j <- jsonlite::toJSON(d)
+ 
   
-  r2d3(d3data, script = "d3/heatmap.js")
+  r2d3(j, script = "d3/heatmap.js", 
+       options = list(breaks = c(10, 50, 100, 500, 1000),
+                      colors = c("#FFFFFF", RColorBrewer::brewer.pal(6, "Greens")),
+                      xaxis = timepoints_xaxis,
+                      yaxis = assays)
+       )
 }
